71711844f6b90c9008cb5f159440fb380a5a9bf2
kipibuzz <142256267+kipibuzz@users.noreply.github.com>
2023-08-18 04:35:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 # Snowflake connection parameters
 CONNECTION_PARAMETERS = {
