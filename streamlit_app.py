13b5c4e50a7414a5b09261662c9f58fd21d7d19a
kipibuzz <142256267+kipibuzz@users.noreply.github.com>
2023-08-18 02:15:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,9 @@ session = Session.builder.configs(CONNECTION_PARAMETERS).create()
 # ... (import statements and Snowflake configuration)
 
 # Verify the code and mark attendance
+# ... (import statements and Snowflake configuration)
+
+# Verify the code and mark attendance
 def verify_and_mark_attendance(verification_code):
     attendees = session.read.table("EMP")
     filtered_attendee = attendees.filter(attendees["CODE"] == verification_code).filter(~attendees["ATTENDED"])
@@ -48,7 +51,7 @@ def verify_and_mark_attendance(verification_code):
         
         # Mark attendee as attended using the DataFrame API
         attendees_to_update = attendees.filter(attendees["CODE"] == verification_code).filter(~attendees["ATTENDED"])
-        attendees_to_update = attendees_to_update.withColumn("ATTENDED", True)
+        attendees_to_update = attendees_to_update.withColumn("ATTENDED", attendees_to_update["ATTENDED"].expr(True))
         
         # Update the attendee table by merging the changes
         attendees.merge(attendees_to_update, on=["ATTENDEE_ID"])
