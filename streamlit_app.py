9d4fad431d067cf34b474f3d30bd32ff9c7e142f
kipibuzz <142256267+kipibuzz@users.noreply.github.com>
2023-08-18 02:36:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,9 @@
 import os
-import glob
 import numpy as np
-import pandas as pd
 import snowflake.connector
 from snowflake.connector.pandas_tools import write_pandas
 from snowflake.snowpark.session import Session
 import streamlit as st
-import pandas as pd
 
 # snowpark connection
 CONNECTION_PARAMETERS = {
@@ -24,21 +21,17 @@ session = Session.builder.configs(CONNECTION_PARAMETERS).create()
 # Verify the code and mark attendance
 def verify_and_mark_attendance(verification_code):
     attendees = session.read.table("EMP")
-    print("Attendees:")
-    attendees.show()
     filtered_attendee = attendees.filter(attendees["CODE"] == verification_code).filter(~attendees["ATTENDED"])
-    print("Filtered Attendees:")
-    filtered_attendee.show()
+    
     if len(filtered_attendee.collect()) > 0:
         attendee_id = filtered_attendee.collect()[0]["ATTENDEE_ID"]
-        print("Matching Attendee ID:", attendee_id)
-        attendees_to_update = attendees.withColumn("ATTENDED", True).filter(attendees["ATTENDEE_ID"] == attendee_id)
-        print("Attendees to Update:")
-        attendees_to_update.show()
+        
+        # Update the attendance status
+        attendees_to_update = attendees.withColumn("ATTENDED", attendees["ATTENDEE_ID"] == attendee_id)
         attendees_to_update.write.overwrite()
+        
         return attendee_id
     else:
-        print("Invalid Code")
         return None
 
 # Streamlit app
@@ -49,9 +42,5 @@ if st.button('Verify'):
         attendee_id = verify_and_mark_attendance(verification_code)
         if attendee_id is not None:
             st.success(f'Code verified successfully for Attendee ID: {attendee_id}! They are marked as attended.')
-            # Increment statistics in Event_Statistics table
-            session.execute(
-                f"UPDATE EVENT_STATISTICS SET TOTAL_VERIFIED = TOTAL_VERIFIED + 1, TOTAL_ATTENDED = TOTAL_ATTENDED + 1 WHERE EVENT_DATE = CURRENT_DATE()"
-            )
         else:
             st.error('Invalid code.')
