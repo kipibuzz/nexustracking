5b6b3c6f8470c961e66e91d49be387c249d2f9bf
kipibuzz <142256267+kipibuzz@users.noreply.github.com>
2023-08-18 02:49:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,31 @@
-import os
-import snowflake.connector
-from snowflake.snowpark.session import Session
 import streamlit as st
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
+from snowflake.snowpark.session import Session
 
-# snowpark connection
+# Snowflake connection parameters
 CONNECTION_PARAMETERS = {
     "account": st.secrets['account'], 
     "user": st.secrets['user'],
     "password": st.secrets['password'],
     "database": st.secrets['database'],
     "schema": st.secrets['schema'],
-    "warehouse": st.secrets['warehouse'], 
+    "warehouse": st.secrets['warehouse'],
 }
 
-# create session
+# Create Snowpark session
 session = Session.builder.configs(CONNECTION_PARAMETERS).create()
 
-# Verify the code and mark attendance
+# Function to verify and mark attendance
 def verify_and_mark_attendance(verification_code):
     attendees = session.read.table("EMP")
-    filtered_attendee = attendees.filter(attendees["CODE"] == verification_code).filter(~attendees["ATTENDED"])
-    
-    if len(filtered_attendee.collect()) > 0:
-        attendee_id = filtered_attendee.collect()[0]["ATTENDEE_ID"]
-        
-        # Update the attendance status using Snowflake SQL
-        query = (
-            f"UPDATE EMP SET ATTENDED = TRUE WHERE ATTENDEE_ID = '{attendee_id}'"
-        )
-        session.sql(query)
-        
+    attendee = attendees.filter(attendees["CODE"] == verification_code).take(1)
+
+    if len(attendee) > 0 and not attendee[0]["ATTENDED"]:
+        attendee_id = attendee[0]["ATTENDEE_ID"]
+        attendees_to_update = attendees.filter(attendees["ATTENDEE_ID"] == attendee_id)
+        attendees_to_update = attendees_to_update.withColumn("ATTENDED", True)
+        attendees_to_update.collect()  # Materialize the update
         return attendee_id
     else:
         return None
@@ -42,5 +38,22 @@ if st.button('Verify'):
         attendee_id = verify_and_mark_attendance(verification_code)
         if attendee_id is not None:
             st.success(f'Code verified successfully for Attendee ID: {attendee_id}! They are marked as attended.')
+
+            # Update statistics in EVENT_STATISTICS table
+            event_date = session.execute(f"SELECT CURRENT_DATE()")[0][0]
+            session.execute(
+                f"MERGE INTO EVENT_STATISTICS "
+                f"USING (SELECT '{event_date}' AS EVENT_DATE) "
+                f"ON EVENT_STATISTICS.EVENT_DATE = '{event_date}' "
+                "WHEN MATCHED THEN "
+                "UPDATE SET TOTAL_VERIFIED = TOTAL_VERIFIED + 1, TOTAL_ATTENDED = TOTAL_ATTENDED + 1 "
+                "WHEN NOT MATCHED THEN "
+                f"INSERT (EVENT_DATE, TOTAL_VERIFIED, TOTAL_ATTENDED) VALUES ('{event_date}', 1, 1)"
+            )
+
         else:
-            st.error('Invalid code.')
+            st.error('Invalid code or attendance already marked.')
+
+# Display event statistics
+statistics = session.read.table("EVENT_STATISTICS")
+st.write(statistics)
