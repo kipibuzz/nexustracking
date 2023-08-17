9859d396e3159c91ba71bd326a2d1d8fe7c8945f
kipibuzz <142256267+kipibuzz@users.noreply.github.com>
2023-08-18 04:25:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,6 +101,8 @@ if menu_choice == "Verify Attendance":
 
 # 
 
+# ... (existing code)
+
 elif menu_choice == "Attendance Statistics":
     # Attendance statistics page
     st.header('Attendance Statistics')
@@ -112,34 +114,24 @@ elif menu_choice == "Attendance Statistics":
     statistics = generate_attendance_statistics(attendance_data)
 
     total_attended = statistics['Total Attended']
-    total_not_attended = statistics['Total Not Attended']
     
-    # Create a bar-like visualization for attended and not attended
+    # Create a bold and large visualization for total attended
     st.write(
-        f"<div style='display: flex; justify-content: space-between; align-items: center;'>"
         f"<div style='text-align: center;'>"
-        f"<h2 style='font-size: 2.5rem; color: green;'>{total_attended}</h2>"
+        f"<h2 style='font-size: 3rem; color: green;'>{total_attended}</h2>"
         f"<p style='font-size: 1.5rem;'>Attended</p>"
-        f"</div>"
-        f"<div style='text-align: center;'>"
-        f"<h2 style='font-size: 2.5rem; color: red;'>{total_not_attended}</h2>"
-        f"<p style='font-size: 1.5rem;'>Not Attended</p>"
-        f"</div>"
         f"</div>",
         unsafe_allow_html=True
     )
     
     # Create a pie chart for attendance breakdown
     plt.figure(figsize=(6, 6))
-    plt.pie(statistics.values(), labels=statistics.keys(), autopct='%1.1f%%', colors=["#86bf91", "#f1c40f", "#e74c3c"])
+    plt.pie([total_attended, statistics['Total Not Attended']], labels=["Attended", "Not Attended"], autopct='%1.1f%%', colors=["#86bf91", "#e74c3c"])
     plt.axis('equal')  # Equal aspect ratio ensures the pie is circular.
     
     st.subheader('Attendance Breakdown')
     st.pyplot(plt)
 
-    
-    st.subheader('Attendance Breakdown')
-    st.pyplot(plt)
 
 
 
